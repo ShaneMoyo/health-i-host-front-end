ef0972b91e28b85035e37f87628856f230a1791a
Shane Moyo <smoyo@pdx.edu>
2018-01-25 20:34:22 -0800

    adjust app.js import Routes

--- src/app/App.js
+++ src/app/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Routes from './Routes';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <head>
+            <meta charset="utf-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css"/>
+            <title>HealthiHost</title>
+          </head>
+
+          <Routes/>
+        </div>
+      </Router>
     );
   }
 }
 
-export default App;
+export default connect(({ auth }) => ({
+  error: auth.error
+}),
+null
+)(App);
